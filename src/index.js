fd6071618470358482e96d86a273b3aae96236fc
Binghui Liu <bertieliu52@gmail.com>
2020-06-30 14:30:32 +0300

    fix path

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
-const User = require("./db");
 
 const connectionString = process.env.MONGODB_URL;
 
