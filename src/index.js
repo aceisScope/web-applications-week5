b5a8a8828a6c517b4c2cb129d2bc4d40d462479a
Binghui Liu <bertieliu52@gmail.com>
2020-07-03 20:15:39 +0300

    set up mongo url with env values

--- src/index.js
+++ src/index.js
@@ -1,14 +1,54 @@
 const express = require("express");
-const router = express.Router();
 const mongoose = require("mongoose");
 const rules = require("./rules");
 const Board = require("./board");
 
-const connectionString = process.env.MONGODB_URL;
+// Reading env variables (config example from https://github.com/sclorg/nodejs-ex/blob/master/server.js)
+var mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
+  mongoURLLabel = "";
 
-mongoose.connect(connectionString);
+// For local dev
+// var mongoURL = 'mongodb://localhost:27017/demodb';
+
+if (mongoURL == null) {
+  var mongoHost, mongoPort, mongoDatabase, mongoPassword, mongoUser;
+  // If using plane old env vars via service discovery
+  if (process.env.DATABASE_SERVICE_NAME) {
+    var mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase();
+    mongoHost = process.env[mongoServiceName + "_SERVICE_HOST"];
+    mongoPort = process.env[mongoServiceName + "_SERVICE_PORT"];
+    mongoDatabase = process.env[mongoServiceName + "_DATABASE"];
+    mongoPassword = process.env[mongoServiceName + "_PASSWORD"];
+    mongoUser = process.env[mongoServiceName + "_USER"];
+
+    // If using env vars from secret from service binding
+  } else if (process.env.database_name) {
+    mongoDatabase = process.env.database_name;
+    mongoPassword = process.env.password;
+    mongoUser = process.env.username;
+    var mongoUriParts = process.env.uri && process.env.uri.split("//");
+    if (mongoUriParts.length === 2) {
+      mongoUriParts = mongoUriParts[1].split(":");
+      if (mongoUriParts && mongoUriParts.length === 2) {
+        mongoHost = mongoUriParts[0];
+        mongoPort = mongoUriParts[1];
+      }
+    }
+  }
+
+  if (mongoHost && mongoPort && mongoDatabase) {
+    if (mongoUser && mongoPassword) {
+      mongoURLLabel = mongoURL = "mongodb://";
+      mongoURL += mongoUser + ":" + mongoPassword + "@";
+    }
+    mongoURLLabel += mongoHost + ":" + mongoPort + "/" + mongoDatabase;
+    mongoURL += mongoHost + ":" + mongoPort + "/" + mongoDatabase;
+  }
+}
+
+mongoose.connect(mongoURL);
 mongoose.connection.on("connected", () => {
-  console.log("connected to database");
+  console.log("connected to database" + mongoURLLabel);
 });
 mongoose.connection.on("error", err => {
   console.log("database error " + err);
